7b6035e1f884d2b405b7fd23d9772b3a631c0341
bhaiwork <bhaiwork22plus@gmail.com>
2023-05-31 14:47:21 +0500

    updates new

--- pages/checkout/index.tsx
+++ pages/checkout/index.tsx
@@ -3,8 +3,9 @@ export default  function App() {
  
 
   return (
-    <div className="App">
-      
+    <div className="py-96 mt-40">
+      <h1 className="text-center text-3xl font-bold italic">Page is under construction due to shortage of labour.</h1>
+      <h2 className="text-center text-md font-bold">Please wait ...</h2>
     </div>
   );
 }
